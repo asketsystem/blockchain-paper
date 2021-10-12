7601c45a84fde963dab9685c2f2be2ec46a86290
asketsystem <32412954+asketsystem@users.noreply.github.com>
2021-10-12 08:16:13 +0000

    Ide shows error with pragma but compile works fine

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -1,5 +1,6 @@
-progma solidity ^0.7.0;
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.5.16;
 
 contract Token {
-    string public name = "AskCoin";       
+    string public name = "AskCoin";
 }
